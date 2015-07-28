b38ff6506c92ba8d16440a5c007d55d2297c476a
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-07-28 12:47:57 +0100

    Added ship_spec

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,8 +10,6 @@ describe Ship do
 	it 'is not placed when created' do
 		expect(subject.placed).to eq(false)
 	end
-# when its created, it is not placed
 
-# 	after it has been placed, it is placed
 
 end
